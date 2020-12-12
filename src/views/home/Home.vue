c47065f146f2f8ed017e9be486e6c2da54ac9a26
zhengxiaoyan111 <1159783954@qq.com>
2020-12-13 07:50:23 +0800

    '首页tarbar和swiper'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="home">
+    <!--    顶部navbar-->
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+
+  <!--    轮播图-->
+    <home-swiper :banners="banners"></home-swiper>
+
+  </div>
+</template>
+
+<script>
+import NavBar from "../../components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+
+import {getHomeMultidata} from "../../network/home";
+
+
+
+export default {
+  name: "Home",
+  components:{
+    NavBar,
+    HomeSwiper
+  },
+  data(){
+    return{
+      banners:[],
+      recommends:[]
+    }
+  },
+  created() {
+  //1.请求多个数据
+    getHomeMultidata().then(res=>{
+      console.log(res.data);
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend;
+      })
+  }
+
+}
+</script>
+
+<style scoped>
+#home {
+  /*padding-top: 44px;*/
+  height: 100vh;
+  position: relative;
+}
+.home-nav{
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,40 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" v-bind:key="item">
+      <a :href="item.link">
+        <img :src="item.image" alt=""/>
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+
+
+import Swiper from "../../../components/common/swiper/Swiper";
+import SwiperItem from "../../../components/common/swiper/SwiperItem";
+// import Swiper from "components/common/swiper";
+// import SwiperItem from "components/common/swiper";
+// import Swiper from "../../components/common/swiper/Swiper";
+// import SwiperItem from "../../components/common/swiper/SwiperItem";
+export default {
+name: "HomeSwiper",
+  props:{
+  banners:{
+    type:Array,
+    default(){
+      return []
+    }
+  }
+  },
+  components:{
+    Swiper,
+    SwiperItem
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
